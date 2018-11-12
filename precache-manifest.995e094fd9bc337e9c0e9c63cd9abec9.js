e721f497ee2c49f2abd7d5225f4c2d8d99a188d8
Joseph Haaga <josephhaaga@Josephs-MacBook-Pro.local>
2018-11-12 14:02:19 -0500

    move build folder contents into root

---
+++ precache-manifest.995e094fd9bc337e9c0e9c63cd9abec9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36604746ef129297b13b",
+    "url": "/MetamodelWebapp/static/js/runtime~main.36604746.js"
+  },
+  {
+    "revision": "8b5161080171498484d7",
+    "url": "/MetamodelWebapp/static/js/main.8b516108.chunk.js"
+  },
+  {
+    "revision": "21851991d7d725c0b1d0",
+    "url": "/MetamodelWebapp/static/js/1.21851991.chunk.js"
+  },
+  {
+    "revision": "8b5161080171498484d7",
+    "url": "/MetamodelWebapp/static/css/main.24a1a311.chunk.css"
+  },
+  {
+    "revision": "77c9264194e169a1d474c1eb116cbc3d",
+    "url": "/MetamodelWebapp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/MetamodelWebapp/precache-manifest.995e094fd9bc337e9c0e9c63cd9abec9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MetamodelWebapp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
