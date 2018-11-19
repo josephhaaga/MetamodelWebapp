53cc74d55880a6fb8a55006142a0964a5fd7fdc2
Joseph Haaga <josephhaaga@Josephs-MacBook-Pro.local>
2018-11-19 12:01:50 -0500

    one-hop example demo complete

--- src/OneHop.js
+++ src/OneHop.js
@@ -1,15 +1,92 @@
 import React, { Component } from 'react';
 
 class OneHop extends Component {
+  constructor(props){
+    super(props)
+    this.data = {
+      motif: "(a)-[r1]->(b); (c)-[r2]->(b); (a) != (c)",
+      transform: "(a)-[r3]->(c)",
+      parameterTypes: {
+        vertex: ["Person", "Company"],
+        edge: ["claims_dependent", "owned_by", "employed_by"]
+      }
+    }
+  }
   render(){
-    return (
+    const vertexOptions = this.data.parameterTypes.vertex.map(x =>
+      <option value={x}>{x}</option>
+    )
+    const edgeOptions = this.data.parameterTypes.edge.map(x =>
+      <option value={x}>{x}</option>
+    )
+    const nodeA = (
+      <div className="node a">
+        <select name="a">
+          {vertexOptions}
+        </select>
+      </div>
+    )
+    const nodeB = (
+      <div className="node b">
+        <select name="b">
+          {vertexOptions}
+        </select>
+      </div>
+    )
+    const nodeC = (
+      <div className="node c">
+        <select name="c">
+          {vertexOptions}
+        </select>
+      </div>
+    )
+    const edgeR1 = (
+      <div className="edge r1">
+        <select name="r1">
+          {edgeOptions}
+        </select>
+      </div>
+    )
+    const edgeR2 = (
+      <div className="edge r2">
+        <select name="r2">
+          {edgeOptions}
+        </select>
+      </div>
+    )
+    const edgeR3 = (
       <div>
-          <li className="accordion-item static" style={{listStyleType: "none"}}>
-            <h4 className="accordion-title">One Hop</h4>
-            <div className="accordion-content">
-              <img src="assets/OneHop.jpg" />
+        <input type="text" />
+      </div>
+    )
+    return (
+      <div className="one-hop">
+        <li className="accordion-item static" style={{listStyleType: "none"}}>
+          <h4 className="accordion-title">One Hop</h4>
+          <div className="accordion-content">
+            <img src="assets/OneHop.jpg" />
+            <div className="inputs">
+              <form className="nodeA">
+                {nodeA}
+              </form>
+              <form className="nodeB">
+                {nodeB}
+              </form>
+              <form className="nodeC">
+                {nodeC}
+              </form>
+              <form className="edgeR1">
+                {edgeR1}
+              </form>
+              <form className="edgeR2">
+                {edgeR2}
+              </form>
+              <form className="edgeR3">
+                {edgeR3}
+              </form>
             </div>
-          </li>
+          </div>
+        </li>
       </div>
     )
   }
