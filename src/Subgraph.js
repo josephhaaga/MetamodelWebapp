6ee36a2df44c24eb94725eb8667d4a7c05a813f6
Joseph Haaga <josephhaaga@Josephs-MacBook-Pro.local>
2018-11-28 17:55:31 -0500

    styled subfilters

--- src/Subgraph.js
+++ src/Subgraph.js
@@ -52,10 +52,72 @@ class Subgraph extends Component {
                 </select>
               </div>
               <div className="cell small-4">
-                <input className="attribute" type="text" value="30"/>
+                <input style={{background: "#bb7149", color: "white"}} type="text" placeholder="30"/>
               </div>
             </div>
           </div>
+          <div className="cell small-11 small-offset-1">
+            <div className="grid-x">
+              <div className="cell small-4">
+                <select className="attribute">
+                  <option value="" disabled>Attr</option>
+                  <option>Name</option>
+                  <option>Age</option>
+                  <option>Status</option>
+                  <option selected>Address</option>
+                </select>
+              </div>
+              <div style={{textAlign: "center"}} className="cell small-4">
+                <select className="logic" style={{paddingRight: "0"}}>
+                  <option value="" disabled>comparison</option>
+                  <option>equals</option>
+                  <option disabled>less than</option>
+                  <option disabled>greater than</option>
+                  <option selected>within</option>
+                </select>
+              </div>
+              <div className="cell small-4">
+                <select style={{paddingRight: "0", background: "#00d170"}}>
+                  <option value="" disabled>Geo</option>
+                  <option>Alabama</option>
+                  <option>Alaska</option>
+                  <option selected>Maryland</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="cell small-11 small-offset-1">
+            <div className="grid-x">
+              <div className="cell small-4">
+                <select className="attribute">
+                  <option value="" disabled>Attr</option>
+                  <option>Name</option>
+                  <option>Age</option>
+                  <option selected>Status</option>
+                  <option>Address</option>
+                </select>
+              </div>
+              <div style={{textAlign: "center"}} className="cell small-4">
+                <select className="logic" style={{paddingRight: "0"}}>
+                  <option value="" disabled>comparison</option>
+                  <option selected>equals</option>
+                  <option disabled>less than</option>
+                  <option disabled>greater than</option>
+                  <option disabled>within</option>
+                </select>
+              </div>
+              <div className="cell small-4">
+                <select style={{paddingRight: "0", background: "#fbf059", color: "#8c8c8c"}}>
+                  <option value="" disabled>Status</option>
+                  <option selected>Single</option>
+                  <option>Married</option>
+                  <option>Divorced</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
         </div>
       : null
     )
